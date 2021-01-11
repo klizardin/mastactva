48e854294f10e3963f10c134be3658d4b53981ab
sergey klizardin <klizardin@gmail.com>
2021-01-11 16:39:43 +0300

    add template for class implementation

--- MastactvaBase/localdataapicache.cpp
+++ MastactvaBase/localdataapicache.cpp
@@ -5,3 +5,29 @@ LocalDataAPICache::LocalDataAPICache(QObject *parent_ /*= nullptr*/)
     : QObject(parent_)
 {
 }
+
+LocalDataAPICache::~LocalDataAPICache()
+{
+    freeRequests();
+}
+
+
+LocalDataAPICache *LocalDataAPICache::g_localDataAPI = nullptr;
+
+
+void LocalDataAPICache::createInstance(QObject *parent_)
+{
+    if(nullptr == g_localDataAPI)
+    {
+        g_localDataAPI = new LocalDataAPICache(parent_);
+    }
+}
+
+LocalDataAPICache *LocalDataAPICache::getInstance()
+{
+    return g_localDataAPI;
+}
+
+void LocalDataAPICache::freeRequests()
+{
+}
--- MastactvaBase/localdataapicache.h
+++ MastactvaBase/localdataapicache.h
@@ -3,16 +3,80 @@
 
 
 #include <QObject>
+#include <QtSql>
+#include <QVector>
+#include <QList>
+#include "../MastactvaBase/Layout.h"
+#include "../MastactvaBase/requestdata.h"
+#include "../MastactvaBase/netapi.h"
+#include "../MastactvaBase/dbrequestinfo.h"
 
 
 class LocalDataAPICache : public QObject
 {
     Q_OBJECT
+
 public:
     explicit LocalDataAPICache(QObject *parent_ = nullptr);
+    virtual ~LocalDataAPICache() override;
+
+    static void createInstance(QObject *parent_);
+    static LocalDataAPICache *getInstance();
+
+    RequestData *emptyRequest(const QString &requestName_, const QVariant &itemAppId_, const QVariant &itemId_)
+    {
+        return nullptr;
+    }
+
+    void freeRequest(RequestData *&r_)
+    {
+    }
+
+    template<class DataType_>
+    RequestData *getList(const QString &layoutName_,
+                         const QString &procedureName_,
+                         const QStringList &refs_,
+                         const QString &currentRef_,
+                         const QString &parentModel_,
+                         const QString &parentModelJsonFieldName_,
+                         const QVariant &refAppId_,
+                         const QVariant &refValue_,
+                         bool jsonParams_,
+                         const QHash<QString, QVariant> &extraFields_,
+                         bool readonly_
+                         )
+    {
+    }
+
+    template<class DataType_>
+    RequestData *addItem(const QString &layoutName_, const DataType_ *item_, const QHash<QString, QVariant> &extraFields_)
+    {
+        return nullptr;
+    }
+
+    template<class DataType_>
+    RequestData *setItem(const QString &layoutName_, const DataType_ *item_, const QHash<QString, QVariant> &extraFields_)
+    {
+        return nullptr;
+    }
+
+    template<class DataType_>
+    RequestData *delItem(const QString &layoutName_, const DataType_ *item_)
+    {
+        return nullptr;
+    }
 
 signals:
+    void response(int errorCode_, const QString &errorCodeStr_, RequestData *request_, const QJsonDocument &reply_);
+    void error(int errorCode_, const QString &errorCodeStr_, const QJsonDocument &reply_);
 
+protected:
+    void freeRequests();
+
+private:
+    QSqlDatabase m_databaseRW;
+    QSqlDatabase m_databaseRO;
+    static LocalDataAPICache *g_localDataAPI;
 };
 
 
