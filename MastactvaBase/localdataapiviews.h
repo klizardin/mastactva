976fb2a3d70e892d08855ecb59c757a8fb401ebf
sergey klizardin <klizardin@gmail.com>
2021-01-14 16:59:36 +0300

    add LocalDataAPIGelListByRefImpl template

--- MastactvaBase/localdataapiviews.h
+++ MastactvaBase/localdataapiviews.h
@@ -2,5 +2,59 @@
 #define LOCALDATAAPIVIEWS_H
 
 
+#include "../MastactvaBase/dbrequestinfo.h"
+#include "../MastactvaBase/imagesource.h"
+#include "../MastactvaBase/IModel.h"
+#include "../MastactvaBase/Layout.h"
+#include "../MastactvaBase/Model.h"
+
+
+template<class ModelType_>
+class LocalDataAPIGelListByRefImpl : public ILocalDataAPI
+{
+public:
+    LocalDataAPIGelListByRefImpl() = default;
+    virtual ~LocalDataAPIGelListByRefImpl() = default;
+
+    virtual bool canProcess(const LocalDBRequest *r_) const
+    {
+        Q_UNUSED(r_);
+        return false;
+    }
+
+    virtual RequestData *getListImpl(const QString& requestName_, LocalDBRequest *r_)
+    {
+        Q_UNUSED(requestName_);
+        Q_UNUSED(r_);
+        return nullptr;
+    }
+
+    virtual RequestData *addItemImpl(const QString& requestName_, const QVariant &appId_, const QHash<QString, QVariant> &values_, LocalDBRequest *r_)
+    {
+        Q_UNUSED(requestName_);
+        Q_UNUSED(appId_);
+        Q_UNUSED(values_);
+        Q_UNUSED(r_);
+        return nullptr;
+    }
+
+    virtual RequestData *setItemImpl(const QString& requestName_, const QVariant &id_, const QHash<QString, QVariant> &values_, LocalDBRequest *r_)
+    {
+        Q_UNUSED(requestName_);
+        Q_UNUSED(id_);
+        Q_UNUSED(values_);
+        Q_UNUSED(r_);
+        return nullptr;
+    }
+
+    virtual RequestData *delItemImpl(const QString& requestName_, const QVariant &id_, LocalDBRequest *r_)
+    {
+        Q_UNUSED(requestName_);
+        Q_UNUSED(id_);
+        Q_UNUSED(r_);
+        return nullptr;
+    }
+};
+
 
 #endif // LOCALDATAAPIVIEWS_H
