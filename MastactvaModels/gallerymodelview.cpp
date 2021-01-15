4c33ddc95b59362488adb9ff54e1b1f5c99f479e
sergey klizardin <klizardin@gmail.com>
2021-01-15 14:01:38 +0300

    add code template

--- MastactvaModels/gallerymodelview.cpp
+++ MastactvaModels/gallerymodelview.cpp
@@ -1,5 +1,41 @@
 #include "gallerymodelview.h"
 
+
 GalleryModelView::GalleryModelView()
 {
+}
+
+bool GalleryModelView::canProcess(const DBRequestInfo *r_) const
+{
+    Q_UNUSED(r_);
+    return false;
+}
+
+bool GalleryModelView::getListImpl(DBRequestInfo *r_)
+{
+    Q_UNUSED(r_);
+    return false;
+}
+
+bool GalleryModelView::addItemImpl(const QVariant &appId_, const QHash<QString, QVariant> &values_, DBRequestInfo *r_)
+{
+    Q_UNUSED(appId_);
+    Q_UNUSED(values_);
+    Q_UNUSED(r_);
+    return false;
+}
+
+bool GalleryModelView::setItemImpl(const QVariant &id_, const QHash<QString, QVariant> &values_, DBRequestInfo *r_)
+{
+    Q_UNUSED(id_);
+    Q_UNUSED(values_);
+    Q_UNUSED(r_);
+    return false;
+}
+
+bool GalleryModelView::delItemImpl(const QVariant &id_, DBRequestInfo *r_)
+{
+    Q_UNUSED(id_);
+    Q_UNUSED(r_);
+    return false;
 }
--- MastactvaModels/gallerymodelview.h
+++ MastactvaModels/gallerymodelview.h
@@ -2,10 +2,30 @@
 #define GALLERYMODELVIEW_H
 
 
-class GalleryModelView
+#include "../MastactvaBase/IModel.h"
+#include "../MastactvaBase/imagesource.h"
+#include "../MastactvaBase/Layout.h"
+#include "../MastactvaBase/Model.h"
+#include "../MastactvaBase/dbrequestinfo.h"
+#include "../MastactvaModels/gallery.h"
+#include "../MastactvaModels/userstep.h"
+
+
+class GalleryModelView : public ILocalDataAPI
 {
 public:
     GalleryModelView();
+    virtual ~GalleryModelView() = default;
+
+    virtual bool canProcess(const DBRequestInfo *r_) const override;
+    virtual bool getListImpl(DBRequestInfo *r_) override;
+    virtual bool addItemImpl(const QVariant &appId_, const QHash<QString, QVariant> &values_, DBRequestInfo *r_) override;
+    virtual bool setItemImpl(const QVariant &id_, const QHash<QString, QVariant> &values_, DBRequestInfo *r_) override;
+    virtual bool delItemImpl(const QVariant &id_, DBRequestInfo *r_) override;
+
+private:
+    UserStepModel m_userStepModel;
+    GalleryModel m_galleryModel;
 };
 
 #endif // GALLERYMODELVIEW_H
