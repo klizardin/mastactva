5262deb9e170e22a323dc19dc746494fc970fe1b
sergey klizardin <klizardin@gmail.com>
2021-02-05 19:38:47 +0300

    add implementation

--- MastactvaModels/translation.cpp
+++ MastactvaModels/translation.cpp
@@ -1,7 +1,139 @@
 #include "translation.h"
+#include "../MastactvaBase/defines.h"
 
 
-Translation::Translation(QObject *parent_ /*= nullptr*/)
+Translation::Translation(TranslationModel *parent_ /*= nullptr*/)
     : QObject(parent_)
 {
+    m_translationModel = parent_;
+}
+
+int Translation::id() const
+{
+    return m_id;
+}
+
+void Translation::setId(const int &id_)
+{
+    m_id = id_;
+
+    emit idChanged();
+}
+
+int Translation::galleryId() const
+{
+    return m_galleryId;
+}
+
+void Translation::setGalleryId(const int &galleryId_)
+{
+    m_galleryId = galleryId_;
+
+    emit galleryIdChanged();
+}
+
+int Translation::descriptionId() const
+{
+    return m_descriptionId;
+}
+
+void Translation::setDescriptionId(const int &descriptionId_)
+{
+    m_descriptionId = descriptionId_;
+
+    emit descriptionIdChanged();
+}
+
+int Translation::questionId() const
+{
+    return m_questionId;
+}
+
+void Translation::setQuestionId(const int &questionId_)
+{
+    m_questionId = questionId_;
+
+    emit questionIdChanged();
+}
+
+int Translation::answerId() const
+{
+    return m_answerId;
+}
+
+void Translation::setAnswerId(const int &answerId_)
+{
+    m_answerId = answerId_;
+
+    emit answerIdChanged();
+}
+
+QString Translation::text() const
+{
+    return m_text;
+}
+
+void Translation::setText(const QString &text_)
+{
+    m_text = text_;
+
+    emit textChanged();
+}
+
+QDateTime Translation::created() const
+{
+    return m_created;
+}
+
+void Translation::setCreated(const QDateTime &created_)
+{
+    m_created = created_;
+
+    emit createdChanged();
+}
+
+bool Translation::autoTranslate() const
+{
+    return m_autoTranslate;
+}
+
+void Translation::setAutoTranslate(const bool &autoTranslate_)
+{
+    m_autoTranslate = autoTranslate_;
+
+    emit autoTranslateChanged();
+}
+
+QString Translation::langFrom() const
+{
+    return m_langFrom;
+}
+
+void Translation::setLangFrom(const QString &langFrom_)
+{
+    m_langFrom = langFrom_;
+
+    emit langFromChanged();
+}
+
+QString Translation::langTo() const
+{
+    return m_langTo;
+}
+
+void Translation::setLangTo(const QString &langTo_)
+{
+    m_langTo = langTo_;
+
+    emit langToChanged();
+}
+
+
+TranslationModel::TranslationModel(QObject *parent_ /*= nullptr*/)
+    :base(parent_)
+{
+#if defined(TRACE_THREADS)
+    qDebug() << "TranslationModel::TranslationModel()" << QThread::currentThread() << QThread::currentThreadId();
+#endif
+    init(this);
 }
