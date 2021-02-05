b7f09db3b868c155b42004eaf87f0d9cb6c95b0b
sergey klizardin <klizardin@gmail.com>
2021-02-05 15:27:00 +0300

    add implementation for EffectArtefactArgModel

--- MastactvaModels/effectartefactarg.cpp
+++ MastactvaModels/effectartefactarg.cpp
@@ -1,7 +1,56 @@
 #include "effectartefactarg.h"
+#include "../MastactvaBase/defines.h"
 
 
-EffectArtefactArg::EffectArtefactArg(QObject *parent_ /*= nullptr*/)
+EffectArtefactArg::EffectArtefactArg(EffectArtefactArgModel *parent_ /*= nullptr*/)
     : QObject(parent_)
 {
+    m_effectArtefactArgModel = parent_;
+}
+
+int EffectArtefactArg::id() const
+{
+    return m_id;
+}
+
+void EffectArtefactArg::setId(const int &id_)
+{
+    m_id = id_;
+
+    emit idChanged();
+}
+
+int EffectArtefactArg::effectArtefactId() const
+{
+    return m_effectArtefactId;
+}
+
+void EffectArtefactArg::setEffectArtefactId(const int &effectArtefactId_)
+{
+    m_effectArtefactId = effectArtefactId_;
+
+    emit effectArtefactIdChanged();
+}
+
+QString EffectArtefactArg::description() const
+{
+    return m_description;
+}
+
+void EffectArtefactArg::setDescription(const QString &description_)
+{
+    m_description = description_;
+
+    emit descriptionChanged();
+}
+
+
+EffectArtefactArgModel::EffectArtefactArgModel(QObject *parent_ /*= nullptr*/)
+    :base(parent_)
+{
+#if defined(TRACE_THREADS)
+    qDebug() << "EffectArtefactArgModel::EffectArtefactArgModel()" << QThread::currentThread() << QThread::currentThreadId();
+#endif
+    init(this);
+    setAutoCreateChildrenModelsImpl(true);
 }
--- MastactvaModels/effectartefactarg.h
+++ MastactvaModels/effectartefactarg.h
@@ -3,16 +3,114 @@
 
 
 #include <QObject>
+#include "../MastactvaBase/IModel.h"
+#include "../MastactvaBase/imagesource.h"
+#include "../MastactvaBase/Layout.h"
+#include "../MastactvaBase/Model.h"
+
+
+class EffectArtefactArgModel;
 
 
 class EffectArtefactArg : public QObject
 {
     Q_OBJECT
 public:
-    explicit EffectArtefactArg(QObject *parent_ = nullptr);
+    explicit EffectArtefactArg(EffectArtefactArgModel *parent_ = nullptr);
+
+    Q_PROPERTY(int effectArtefactArgId READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(int effectArtefactArgEffectArtefactId READ effectArtefactId WRITE setEffectArtefactId NOTIFY effectArtefactIdChanged)
+    Q_PROPERTY(QString effectArtefactArgDescription READ description WRITE setDescription NOTIFY descriptionChanged)
+
+
+    class DefaultLayout : public LayoutBase<EffectArtefactArg>
+    {
+    public:
+        DefaultLayout()
+        {
+            setLayoutJsonName("effect-artefact-arg");
+            addSpecial<IListModelInfo *>(layout::SpecialFieldEn::modelInfo, &EffectArtefactArg::m_parentModelInfo);
+            addSpecial<IListModelInfo *>(layout::SpecialFieldEn::objectModelInfo, &EffectArtefactArg::m_objectModelInfo);
+            addSpecial<int>(layout::SpecialFieldEn::appId, &EffectArtefactArg::m_appId);
+            addField<int>("id", "effectArtefactArgId", &EffectArtefactArg::id, &EffectArtefactArg::setId);
+            addField<int>("effect_artefact", "effectArtefactArgEffectArtefactId", &EffectArtefactArg::effectArtefactId, &EffectArtefactArg::setEffectArtefactId);
+            addField<QString>("description", "effectArtefactArgDescription", &EffectArtefactArg::description, &EffectArtefactArg::setDescription);
+            setIdField("id");
+        }
+    };
+
+public:
+    int id() const;
+    void setId(const int &id_);
+    int effectArtefactId() const;
+    void setEffectArtefactId(const int &effectArtefactId_);
+    QString description() const;
+    void setDescription(const QString &description_);
 
 signals:
+    void idChanged();
+    void effectArtefactIdChanged();
+    void descriptionChanged();
 
+private:
+    IListModelInfo *m_parentModelInfo = nullptr;
+    IListModelInfo *m_objectModelInfo = nullptr;
+    int m_appId = -1;
+    int m_id = -1;
+    int m_effectArtefactId = -1;
+    QString m_description;
+    EffectArtefactArgModel *m_effectArtefactArgModel = nullptr;
+};
+
+
+class EffectArtefactArgModel : public ListModelBaseOfData<EffectArtefactArg, EffectArtefactArgModel>
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+protected:
+    using base = ListModelBaseOfData<EffectArtefactArg, EffectArtefactArgModel>;
+
+public:
+    explicit EffectArtefactArgModel(QObject *parent_ = nullptr);
+
+    LAYOUT_MODEL_IMPL();
+
+public slots:
+    void jsonResponseSlot(int errorCode_,
+                          const QString& errorCodeStr_,
+                          RequestData *request_,
+                          const QJsonDocument &reply_)
+    {
+        jsonResponseSlotImpl(errorCode_, errorCodeStr_, request_, reply_);
+    }
+
+    void refreshChildrenSlot(QString modelName_)
+    {
+        refreshChildrenSlotImpl(modelName_);
+    }
+
+signals:
+    void currentIndexChanged();
+    void currentItemChanged();
+    void currentRefChanged();
+    void storeAfterSaveChanged();
+    void refAppIdChanged();
+    void refValueChanged();
+    void layoutQMLNameChanged();
+    void layoutIdFieldChanged();
+    void refreshChildren(QString);
+    void jsonParamsGetChanged();
+    void autoCreateChildrenModelsChanged();
+    void autoCreateChildrenModelsOnSelectChanged();
+    void listReloaded();
+    void itemAdded();
+    void itemSet();
+    void itemDeleted();
+    void outputModelChanged();
+    void sortFieldsChanged();
+    void readonlyChanged();
+    void error(const QString &errorCode_, const QString &description_);
 };
 
 
