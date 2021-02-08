eb8da31f12f3772abb786bd34287a3ec099f2912
sergey klizardin <klizardin@gmail.com>
2021-02-08 15:08:49 +0300

    add implementation for ArtefactArgStorageModel

--- MastactvaModels/artefactargstorage.cpp
+++ MastactvaModels/artefactargstorage.cpp
@@ -1,7 +1,45 @@
 #include "artefactargstorage.h"
+#include "../MastactvaBase/defines.h"
 
 
 ArtefactArgStorage::ArtefactArgStorage(QObject *parent_ /*= nullptr*/)
     : QObject(parent_)
 {
+#if defined(TRACE_THREADS)
+    qDebug() << "ArtefactArgStorage::ArtefactArgStorage()" << QThread::currentThread() << QThread::currentThreadId();
+#endif
+}
+
+int ArtefactArgStorage::id() const
+{
+    return m_id;
+}
+
+void ArtefactArgStorage::setId(const int &id_)
+{
+    m_id = id_;
+
+    emit idChanged();
+}
+
+QString ArtefactArgStorage::storage() const
+{
+    return m_storage;
+}
+
+void ArtefactArgStorage::setStorage(const QString &storage_)
+{
+    m_storage = storage_;
+
+    emit storageChanged();
+}
+
+
+ArtefactArgStorageModel::ArtefactArgStorageModel(QObject *parent_ /*= nullptr*/)
+    :base(parent_)
+{
+#if defined(TRACE_THREADS)
+    qDebug() << "ArtefactArgStorageModel::ArtefactArgStorageModel()" << QThread::currentThread() << QThread::currentThreadId();
+#endif
+    init(this);
 }
--- MastactvaModels/artefactargstorage.h
+++ MastactvaModels/artefactargstorage.h
@@ -3,6 +3,10 @@
 
 
 #include <QObject>
+#include "../MastactvaBase/IModel.h"
+#include "../MastactvaBase/imagesource.h"
+#include "../MastactvaBase/Layout.h"
+#include "../MastactvaBase/Model.h"
 
 
 class ArtefactArgStorage : public QObject
@@ -11,8 +15,89 @@ class ArtefactArgStorage : public QObject
 public:
     explicit ArtefactArgStorage(QObject *parent_ = nullptr);
 
-signals:
+    Q_PROPERTY(int artefactArgStorageId READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(QString artefactArgStorageStorage READ storage WRITE setStorage NOTIFY storageChanged)
 
+    class DefaultLayout : public LayoutBase<ArtefactArgStorage>
+    {
+    public:
+        DefaultLayout()
+        {
+            setLayoutJsonName("artefact-arg-storage");
+            addSpecial<IListModelInfo *>(layout::SpecialFieldEn::modelInfo, &ArtefactArgStorage::m_parentModelInfo);
+            addSpecial<int>(layout::SpecialFieldEn::appId, &ArtefactArgStorage::m_appId);
+            addField<int>("id", "artefactArgStorageId", &ArtefactArgStorage::id, &ArtefactArgStorage::setId);
+            addField<QString>("storage", "artefactArgStorageStorage", &ArtefactArgStorage::storage, &ArtefactArgStorage::setStorage);
+            setIdField("id");
+        }
+    };
+
+public:
+    int id() const;
+    void setId(const int &id_);
+    QString storage() const;
+    void setStorage(const QString &storage_);
+
+signals:
+    void idChanged();
+    void storageChanged();
+
+private:
+    IListModelInfo *m_parentModelInfo = nullptr;
+    int m_appId = -1;
+    int m_id = -1;
+    QString m_storage;
+};
+
+
+class ArtefactArgStorageModel : public ListModelBaseOfData<ArtefactArgStorage, ArtefactArgStorageModel>
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+protected:
+    using base = ListModelBaseOfData<ArtefactArgStorage, ArtefactArgStorageModel>;
+
+public:
+    explicit ArtefactArgStorageModel(QObject *parent_ = nullptr);
+
+    LAYOUT_MODEL_IMPL();
+
+public slots:
+    void jsonResponseSlot(int errorCode_,
+                          const QString& errorCodeStr_,
+                          RequestData *request_,
+                          const QJsonDocument &reply_)
+    {
+        jsonResponseSlotImpl(errorCode_, errorCodeStr_, request_, reply_);
+    }
+
+    void refreshChildrenSlot(QString modelName_)
+    {
+        refreshChildrenSlotImpl(modelName_);
+    }
+
+signals:
+    void currentIndexChanged();
+    void currentItemChanged();
+    void currentRefChanged();
+    void storeAfterSaveChanged();
+    void refAppIdChanged();
+    void refValueChanged();
+    void layoutQMLNameChanged();
+    void layoutIdFieldChanged();
+    void refreshChildren(QString);
+    void jsonParamsGetChanged();
+    void autoCreateChildrenModelsChanged();
+    void autoCreateChildrenModelsOnSelectChanged();
+    void listReloaded();
+    void itemAdded();
+    void itemSet();
+    void itemDeleted();
+    void outputModelChanged();
+    void sortFieldsChanged();
+    void readonlyChanged();
+    void error(const QString &errorCode_, const QString &description_);
 };
 
 
