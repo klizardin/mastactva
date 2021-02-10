7dd703fa2aaeb338de76433cd462696b827cfcca
sergey klizardin <klizardin@gmail.com>
2021-02-10 16:55:28 +0300

    add EffectObjectsModel implementation

--- MastactvaModels/effectobjects.cpp
+++ MastactvaModels/effectobjects.cpp
@@ -1,7 +1,102 @@
 #include "effectobjects.h"
+#include "../MastactvaBase/defines.h"
 
 
-EffectObjects::EffectObjects(QObject *parent_ /*= nullptr*/)
+EffectObjects::EffectObjects(EffectObjectsModel *parent_ /*= nullptr*/)
     : QObject(parent_)
 {
+#if defined(TRACE_THREADS)
+    qDebug() << "EffectObjects::EffectObjects()" << QThread::currentThread() << QThread::currentThreadId();
+#endif
+
+    m_effectObjectsModel = parent_;
+}
+
+int EffectObjects::id() const
+{
+    return m_id;
+}
+
+void EffectObjects::setId(const int &id_)
+{
+    m_id = id_;
+
+    emit idChanged();
+}
+
+int EffectObjects::effectId() const
+{
+    return m_effectId;
+}
+
+void EffectObjects::setEffectId(const int &effectId_)
+{
+    m_effectId = effectId_;
+
+    emit effectIdChanged();
+}
+
+int EffectObjects::effectObjectInfoId() const
+{
+    return m_effectObjectInfoId;
+}
+
+void EffectObjects::setEffectObjectInfoId(const int &effectObjectInfoId_)
+{
+    m_effectObjectInfoId = effectObjectInfoId_;
+
+    emit effectObjectInfoChanged();
+}
+
+QVariant EffectObjects::effectObjectInfo() const
+{
+    if(nullptr == m_effectObjectInfoModel)
+    {
+        const_cast<EffectObjects *>(this)->m_effectObjectInfoModel = const_cast<EffectObjects *>(this)
+                ->createEffectObjectInfoModel();
+    }
+    return QVariant::fromValue(static_cast<QObject *>(
+                                   const_cast<EffectObjectInfoModel *>(
+                                       m_effectObjectInfoModel)
+                                   )
+                               );
+}
+
+void EffectObjects::setEffectObjectInfo(const QVariant &obj_)
+{
+    if(obj_.isNull() && nullptr != m_effectObjectInfoModel)
+    {
+        delete m_effectObjectInfoModel;
+        m_effectObjectInfoModel = nullptr;
+
+        emit effectObjectInfoChanged();
+    }
+}
+
+EffectObjectInfoModel *EffectObjects::createEffectObjectInfoModel()
+{
+    EffectObjectInfoModel *m = new EffectObjectInfoModel(this);
+    m->initResponse();
+    m->setLayoutRefImpl("id", m_effectObjectsModel->getQMLLayoutName(), "effect_object_info", false);
+    m->setCurrentRef("id");
+    m->setRefAppId(QVariant::fromValue(m_appId));
+    m->setLayoutQMLName(m_effectObjectsModel->getQMLLayoutName() + QString("_EffectObjects_") +
+                        QVariant::fromValue(m_appId).toString() + QString("_EffectObjectInfoModel_"));
+    m->registerListModel();
+    m->setParentListModelInfo(m_parentModelInfo);
+    m->setAutoCreateChildrenModels(true);
+    m->setReadonlyImpl(false);
+    m->loadList();
+    return m;
+}
+
+
+EffectObjectsModel::EffectObjectsModel(QObject *parent_ /*= nullptr*/)
+    :base(parent_)
+{
+#if defined(TRACE_THREADS)
+    qDebug() << "EffectObjectsModel::EffectObjectsModel()" << QThread::currentThread() << QThread::currentThreadId();
+#endif
+
+    init(this);
 }
--- MastactvaModels/effectobjects.h
+++ MastactvaModels/effectobjects.h
@@ -3,16 +3,122 @@
 
 
 #include <QObject>
+#include "../MastactvaBase/IModel.h"
+#include "../MastactvaBase/imagesource.h"
+#include "../MastactvaBase/Layout.h"
+#include "../MastactvaBase/Model.h"
+#include "../MastactvaModels/effectobjectinfo.h"
+
+
+class EffectObjectsModel;
 
 
 class EffectObjects : public QObject
 {
     Q_OBJECT
 public:
-    explicit EffectObjects(QObject *parent_ = nullptr);
+    explicit EffectObjects(EffectObjectsModel *parent_ = nullptr);
+
+    Q_PROPERTY(int effectObjectsId READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(int effectObjectsEffectId READ effectId WRITE setEffectId NOTIFY effectIdChanged)
+    Q_PROPERTY(QVariant effectObjectsEffectObjectInfo READ effectObjectInfo WRITE setEffectObjectInfo NOTIFY effectObjectInfoChanged)
+
+    class DefaultLayout : public LayoutBase<EffectObjects>
+    {
+    public:
+        DefaultLayout()
+        {
+            setLayoutJsonName("artefact");
+            addSpecial<IListModelInfo *>(layout::SpecialFieldEn::modelInfo, &EffectObjects::m_parentModelInfo);
+            addSpecial<IListModelInfo *>(layout::SpecialFieldEn::objectModelInfo, &EffectObjects::m_objectModelInfo);
+            addSpecial<int>(layout::SpecialFieldEn::appId, &EffectObjects::m_appId);
+            addField<int>("id", "effectObjectsId", &EffectObjects::id, &EffectObjects::setId);
+            addField<int>("effect", "effectObjectsEffectId", &EffectObjects::effectId, &EffectObjects::setEffectId);
+            addField<int>("effect_object_info", "", &EffectObjects::effectObjectInfoId, &EffectObjects::setEffectObjectInfoId);
+            addModel<EffectObjectInfoModel>("effectObjectsEffectObjectInfo", &EffectObjects::m_effectObjectInfoModel, &EffectObjects::createEffectObjectInfoModel);
+            setIdField("id");
+        }
+    };
+
+public:
+    int id() const;
+    void setId(const int &id_);
+    int effectId() const;
+    void setEffectId(const int &effectId_);
+    int effectObjectInfoId() const;
+    void setEffectObjectInfoId(const int &effectObjectInfoId_);
+    QVariant effectObjectInfo() const;
+    void setEffectObjectInfo(const QVariant &obj_);
+
+protected:
+    EffectObjectInfoModel *createEffectObjectInfoModel();
+
 
 signals:
+    void idChanged();
+    void effectIdChanged();
+    void effectObjectInfoChanged();
 
+private:
+    EffectObjectsModel *m_effectObjectsModel = nullptr;
+    IListModelInfo *m_parentModelInfo = nullptr;
+    IListModelInfo *m_objectModelInfo = nullptr;
+    int m_appId = -1;
+    int m_id = -1;
+    int m_effectId = -1;
+    int m_effectObjectInfoId = -1;
+    EffectObjectInfoModel *m_effectObjectInfoModel = nullptr;
+};
+
+
+class EffectObjectsModel : public ListModelBaseOfData<EffectObjects, EffectObjectsModel>
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+protected:
+    using base = ListModelBaseOfData<EffectObjects, EffectObjectsModel>;
+
+public:
+    explicit EffectObjectsModel(QObject *parent_ = nullptr);
+
+    LAYOUT_MODEL_IMPL();
+
+public slots:
+    void jsonResponseSlot(int errorCode_,
+                          const QString& errorCodeStr_,
+                          RequestData *request_,
+                          const QJsonDocument &reply_)
+    {
+        jsonResponseSlotImpl(errorCode_, errorCodeStr_, request_, reply_);
+    }
+
+    void refreshChildrenSlot(QString modelName_)
+    {
+        refreshChildrenSlotImpl(modelName_);
+    }
+
+signals:
+    void currentIndexChanged();
+    void currentItemChanged();
+    void currentRefChanged();
+    void storeAfterSaveChanged();
+    void refAppIdChanged();
+    void refValueChanged();
+    void layoutQMLNameChanged();
+    void layoutIdFieldChanged();
+    void refreshChildren(QString);
+    void jsonParamsGetChanged();
+    void autoCreateChildrenModelsChanged();
+    void autoCreateChildrenModelsOnSelectChanged();
+    void listReloaded();
+    void itemAdded();
+    void itemSet();
+    void itemDeleted();
+    void outputModelChanged();
+    void sortFieldsChanged();
+    void readonlyChanged();
+    void error(const QString &errorCode_, const QString &description_);
 };
 
 
