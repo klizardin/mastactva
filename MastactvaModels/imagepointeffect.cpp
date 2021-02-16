0c2d25d095775f7c1368ee4cc162a77f46c7bc3f
sergey klizardin <klizardin@gmail.com>
2021-02-16 11:39:05 +0300

    refactioring, remove unused comments

--- MastactvaModels/imagepointeffect.cpp
+++ MastactvaModels/imagepointeffect.cpp
@@ -122,18 +122,6 @@ void ImagePointEffect::setArgSet(const QVariant &obj_)
     }
 }
 
-//int ImagePointEffect::stepIndex() const
-//{
-//    return m_stepIndex;
-//}
-
-//void ImagePointEffect::setStepIndex(const int &stepIndex_)
-//{
-//    m_stepIndex = stepIndex_;
-//
-//    emit stepIndexChanged();
-//}
-
 int ImagePointEffect::duration() const
 {
     return m_duration;
