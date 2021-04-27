33719ed19c69043dd4816d9fafb5bd538fc0a4d4
sergey <klizardin@gmail.com>
2021-04-27 11:45:42 +0300

    add pinup test

--- MastactvaTests/dbutils_unittests.h
+++ MastactvaTests/dbutils_unittests.h
@@ -4,6 +4,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 #include <QString>
+#include <QVariant>
+#include <QVariantList>
 #include "../MastactvaBase/dbutils.h"
 #include "../MastactvaBase/names.h"
 
@@ -38,5 +40,30 @@ TEST(DBUtils, equalToValueConditionListFromSqlNameList)
     ASSERT_STRCASEEQ("ref_another_name=:ref_another_name", res[1].toUtf8().constData());
 }
 
+TEST(DBUtils, filterNames)
+{
+    QVariantList filterEmptyList;
+    QStringList names({"some_name", "\"another_name\"", "1"});
+    const QStringList res1 = db::filterNames(
+                names,
+                filterEmptyList
+                );
+    ASSERT_EQ(res1.size(), 3);
+    QVariantList filterList1;
+    filterList1.push_back(QVariant::fromValue(1));
+    const QStringList res2 = db::filterNames(
+                names,
+                filterList1
+                );
+    ASSERT_EQ(res2.size(), 1);
+    QVariantList filterList2;
+    filterList2.push_back(QVariant::fromValue(1));
+    filterList2.push_back(QVariant::fromValue(QString("some_name")));
+    const QStringList res3 = db::filterNames(
+                names,
+                filterList2
+                );
+    ASSERT_EQ(res3.size(), 2);
+}
 
 #endif
