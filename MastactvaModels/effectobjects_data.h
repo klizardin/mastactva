6f5c88d22803c5227cc3aaca567cf07e59bf6a8b
sergey <klizardin@gmail.com>
2021-06-03 09:01:17 +0300

    add in class sub data

--- MastactvaModels/effectobjects_data.h
+++ MastactvaModels/effectobjects_data.h
@@ -2,14 +2,28 @@
 #define EFFECTOBJECTS_DATA_H
 
 
+#include <memory>
+#include <QVector>
+#include "../MastactvaModels/objectinfo_data.h"
+
+
 struct EffectObjectsData
 {
+    EffectObjectsData();
+    EffectObjectsData(EffectObjectsData &&data_);
     virtual ~EffectObjectsData() = default;
+
+    EffectObjectsData & operator = (EffectObjectsData &&data_);
 
     int m_id = -1;
     int m_effectId = -1;
     int m_objectInfoId = -1;
     int m_stepIndex = -1;
+
+    std::shared_ptr<QVector<ObjectInfoData *>> m_objectInfoData;
+
+    EffectObjectsData(const EffectObjectsData &data_) = delete;
+    EffectObjectsData & operator = (const EffectObjectsData &data_) = delete;
 };
 
 
