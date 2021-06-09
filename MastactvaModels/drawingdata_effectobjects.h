c7804b0bff9b1e4bf8359d29f5d4861154d7bbed
sergey <klizardin@gmail.com>
2021-06-09 12:49:23 +0300

    add comments

--- MastactvaModels/drawingdata_effectobjects.h
+++ MastactvaModels/drawingdata_effectobjects.h
@@ -17,10 +17,13 @@ public:
     DrawingDataEffectObjects() = default;
     DrawingDataEffectObjects(EffectObjectsData &&data_);
 
+    // for util code
     EffectObjectsData * getData();
 
+    // DIP initializer
     void init(std::shared_ptr<drawingdata::IFileSource> filesources_);
 
+    // method to form QuizImageObjects data
     void initialize(drawing_data::QuizImageObjects &data_) const override;
 
 private:
