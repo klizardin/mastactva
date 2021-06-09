a61be59492e4ee44bfa0485e4c277fb2dd21ee6c
sergey <klizardin@gmail.com>
2021-06-09 14:17:31 +0300

    add shaders for the base test

--- QuizImageDemoArtefactsStepsFB/object_tests.cpp
+++ QuizImageDemoArtefactsStepsFB/object_tests.cpp
@@ -22,12 +22,47 @@ QImage MapFileSource::getImage(const FileSource &filename_) const
     return m_images.value(filename_, QImage());
 }
 
+
+static const char *g_baseVertexShader =
+    "attribute highp vec4 vertex;\n"
+    "attribute mediump vec3 normal;\n"
+    "uniform mediump mat4 matrix;\n"
+    "varying mediump vec4 color;\n"
+    "void main(void)\n"
+    "{\n"
+    "    vec3 toLight = normalize(vec3(0.0, 0.3, 1.0));\n"
+    "    float angle = max(dot(normal, toLight), 0.0);\n"
+    "    vec3 col = vec3(0.40, 1.0, 0.0);\n"
+    "    color = vec4(col * 0.2 + col * 0.8 * angle, 1.0);\n"
+    "    color = clamp(color, 0.0, 1.0);\n"
+    "    gl_Position = matrix * vertex;\n"
+    "}\n";
+
+static const char *g_baseFragmatShader =
+    "varying mediump vec4 color;\n"
+    "void main(void)\n"
+    "{\n"
+    "    gl_FragColor = color;\n"
+    "}\n";
+
+static const char *g_baseVertexShaderFilename = "base.vsh";
+static const char *g_baseFragmentShaderFilename = "base.fsh";
+
+std::shared_ptr<MapFileSource> createMapFileSource()
+{
+    std::shared_ptr<MapFileSource> filesource = std::make_shared<MapFileSource>();
+    filesource->add(g_baseVertexShaderFilename, g_baseVertexShader);
+    filesource->add(g_baseFragmentShaderFilename, g_baseFragmatShader);
+    return filesource;
+}
+
+
 namespace drawing_objects
 {
 
 void BaseTest::initialize(drawing_data::QuizImageObjects &data_) const
 {
-    std::shared_ptr<MapFileSource> filesource = std::make_shared<MapFileSource>();
+    std::shared_ptr<MapFileSource> filesource = createMapFileSource();
     ::EffectObjectsData effectObjectData;
     ::DrawingDataEffectObjects drawingDataEffectObjects(std::move(effectObjectData));
     drawingDataEffectObjects.init(filesource);
