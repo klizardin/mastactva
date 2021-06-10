887529e4519fe4d3a83a134c4a28a72e4ef9881f
sergey <klizardin@gmail.com>
2021-06-10 15:14:54 +0300

    add proxy implementation

--- MastactvaModels/drawingdata_objectartefact.cpp
+++ MastactvaModels/drawingdata_objectartefact.cpp
@@ -1,6 +1,4 @@
 #include "drawingdata_objectartefact.h"
-
-
 #include "../MastactvaBase/drawingdata_utils.h"
 #include "../MastactvaModels/drawingdata_artefact.h"
 
@@ -19,4 +17,65 @@ ObjectArtefactData * DrawingDataObjectArtefact::getData()
 int DrawingDataObjectArtefact::getStepIndex() const
 {
     return m_stepIndex;
+}
+
+bool DrawingDataObjectArtefact::forArtefacts(
+        std::function<bool(const DrawingDataArtefact *)> func_
+        ) const
+{
+    if(!m_artefactData.operator bool())
+    {
+        return false;
+    }
+    for(const ArtefactData * artefact_: *m_artefactData)
+    {
+        auto artefact = dynamic_cast<const DrawingDataArtefact *>(artefact_);
+        if(!artefact)
+        {
+            continue;
+        }
+        if(func_(artefact))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool DrawingDataObjectArtefact::setVertexShader(
+        drawing_data::QuizImageObject &object_,
+        drawingdata::IFileSource *filesource_
+        ) const
+{
+    return forArtefacts([&object_, &filesource_](const DrawingDataArtefact *artefact_)->bool
+    {
+        return artefact_->setVertexShader(object_, filesource_);
+    });
+}
+
+bool DrawingDataObjectArtefact::setFragmentShader(
+        drawing_data::QuizImageObject &object_,
+        drawingdata::IFileSource *filesource_
+        ) const
+{
+    return forArtefacts([&object_, &filesource_](const DrawingDataArtefact *artefact_)->bool
+    {
+        return artefact_->setFragmentShader(object_, filesource_);
+    });
+}
+
+void DrawingDataObjectArtefact::addArguments(
+        drawing_data::QuizImageObject &object_
+        ) const
+{
+    (void)forArtefacts([&object_](const DrawingDataArtefact *artefact_)->bool
+    {
+        artefact_->addArguments(object_);
+        return false;
+    });
+}
+
+bool operator < (const DrawingDataObjectArtefact &left_, const DrawingDataObjectArtefact &right_)
+{
+    return left_.m_stepIndex < right_.m_stepIndex;
 }
