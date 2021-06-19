c0ac645040611c1a210f441f7fcfb669a60f0c6a
sergey <klizardin@gmail.com>
2021-06-19 16:38:42 +0300

    add projects to setup lua api tests

---
+++ ArtefactLuaTests/tst_installed.h
@@ -0,0 +1,15 @@
+#ifndef TST_INSTALLED_H
+#define TST_INSTALLED_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
+
+TEST(GTest, installed)
+{
+    EXPECT_EQ(1, 1);
+    ASSERT_THAT(0, Eq(0));
+}
+
+#endif // TST_INSTALLED_H
