32406fbc4742f8ded3aabd62e19be111d08bec89
sergey <klizardin@gmail.com>
2021-07-01 08:44:43 +0300

    correct interface method

--- MastactvaBase/opengldrawing_utils.h
+++ MastactvaBase/opengldrawing_utils.h
@@ -15,7 +15,7 @@ public:
     virtual ~IVariables() = default;
     virtual bool get(const QString &name_, QVector<double> &data_) const = 0;
     virtual bool set(const QString &name_, const QVector<double> &data_) = 0;
-    virtual bool set(const QString &name_, QVector<double> &&data_) = 0;
+    virtual void set(const QString &name_, QVector<double> &&data_) = 0;
 };
 
 }
