be3e7731d40648066b281c7cbb220a1802080a98
sergey <klizardin@gmail.com>
2021-07-01 08:42:11 +0300

    add interface IVariables

--- MastactvaBase/opengldrawing_utils.h
+++ MastactvaBase/opengldrawing_utils.h
@@ -2,5 +2,23 @@
 #define OPENGLDRAWING_UTILS_H
 
 
+#include <QString>
+#include <QVector>
+
+
+namespace opengl_drawing
+{
+
+class IVariables
+{
+public:
+    virtual ~IVariables() = default;
+    virtual bool get(const QString &name_, QVector<double> &data_) const = 0;
+    virtual bool set(const QString &name_, const QVector<double> &data_) = 0;
+    virtual bool set(const QString &name_, QVector<double> &&data_) = 0;
+};
+
+}
+
 
 #endif // OPENGLDRAWING_UTILS_H
