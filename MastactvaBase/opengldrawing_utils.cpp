b21a52bcf3c007ea88e6732e26f1282e5ae85467
sergey <klizardin@gmail.com>
2021-07-01 15:03:27 +0300

    add class to extend interface

--- MastactvaBase/opengldrawing_utils.cpp
+++ MastactvaBase/opengldrawing_utils.cpp
@@ -7,6 +7,43 @@
 namespace opengl_drawing
 {
 
+VariablesExtended::VariablesExtended(IVariables *base_, IVariables *extend_)
+    : m_base(base_),
+      m_extend(extend_)
+{
+}
+
+bool VariablesExtended::get(const QString &name_, QVector<double> &data_) const
+{
+    return  m_base ?
+                m_base->get(name_, data_)
+              : m_extend ?
+                    m_extend->get(name_, data_)
+                  : false
+                    ;
+}
+
+void VariablesExtended::set(const QString &name_, const QVector<double> &data_)
+{
+    m_base ?
+        m_base->set(name_, data_)
+        : m_extend ?
+            m_extend->set(name_, data_)
+          : static_cast<void>(nullptr)
+            ;
+}
+
+void VariablesExtended::set(const QString &name_, QVector<double> &&data_)
+{
+    m_base ?
+        m_base->set(name_, std::move(data_))
+        : m_extend ?
+            m_extend->set(name_, std::move(data_))
+          : static_cast<void>(nullptr)
+            ;
+}
+
+
 const QString &IEffectCalculation::getFilename() const
 {
     return m_filename;
--- MastactvaBase/opengldrawing_utils.h
+++ MastactvaBase/opengldrawing_utils.h
@@ -19,6 +19,21 @@ public:
     virtual void set(const QString &name_, QVector<double> &&data_) = 0;
 };
 
+class VariablesExtended : public IVariables
+{
+public:
+    VariablesExtended(IVariables *base_, IVariables *extend_);
+
+    bool get(const QString &name_, QVector<double> &data_) const override;
+    void set(const QString &name_, const QVector<double> &data_) override;
+    void set(const QString &name_, QVector<double> &&data_) override;
+
+private:
+    IVariables *m_base = nullptr;
+    IVariables *m_extend = nullptr;
+};
+
+
 // stateless (except this members)
 class IEffectCalculation
 {
