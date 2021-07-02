cdeaefac33aac0878ea25c7b4a7dff4fcb97429f
sergey <klizardin@gmail.com>
2021-07-02 15:54:50 +0300

    optimization

--- MastactvaBase/opengldrawing_utils.h
+++ MastactvaBase/opengldrawing_utils.h
@@ -5,6 +5,7 @@
 #include <QString>
 #include <QVector>
 #include <QStringList>
+#include <QSet>
 
 
 namespace opengl_drawing
@@ -17,7 +18,7 @@ public:
     virtual bool get(const QString &name_, QVector<double> &data_) const = 0;
     virtual void set(const QString &name_, const QVector<double> &data_) = 0;
     virtual void set(const QString &name_, QVector<double> &&data_) = 0;
-    virtual bool isUpdated(const QStringList &vars_, IVariables *base_) const = 0;
+    virtual bool isUpdated(const QSet<QString> &vars_, IVariables *base_) const = 0;
 };
 
 class VariablesExtended : public IVariables
@@ -28,7 +29,7 @@ public:
     bool get(const QString &name_, QVector<double> &data_) const override;
     void set(const QString &name_, const QVector<double> &data_) override;
     void set(const QString &name_, QVector<double> &&data_) override;
-    bool isUpdated(const QStringList &vars_, IVariables *base_) const override;
+    bool isUpdated(const QSet<QString> &vars_, IVariables *base_) const override;
 
 private:
     IVariables *m_base = nullptr;
@@ -45,7 +46,7 @@ public:
 
     const QString &getFilename() const;
     bool doExtend(IEffectCalculation *calculation_) const;
-    const QStringList &getRequiredVariables() const;
+    const QSet<QString> &getRequiredVariables() const;
 
 protected:
     void setFilename(const QString &filename_);
@@ -53,7 +54,7 @@ protected:
 
 private:
     QString m_filename;
-    QStringList m_vars;
+    QSet<QString> m_vars;
 };
 
 }
