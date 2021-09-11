0b0e726d03bed707926cdd9a54fd971a3dba56f0
sergey <klizardin@gmail.com>
2021-09-11 14:32:55 +0300

    refactoring - move class to separate source files

--- MastactvaModels/effectargvalue_data.cpp
+++ MastactvaModels/effectargvalue_data.cpp
@@ -1,2 +1,38 @@
 #include "effectargvalue_data.h"
+#include "../MastactvaBase/data_utils.h"
 
+
+EffectArgValueData::EffectArgValueData()
+{
+    m_effectArgsData = ::data_object::utils::createDataVector(static_cast<const EffectArgData *>(nullptr));
+}
+
+EffectArgValueData::EffectArgValueData(
+        int id_,
+        int argSetId_,
+        int argId_,
+        const QString &value_,
+        const QString &description_,
+        const QDateTime &created_
+        )
+    : m_id(id_),
+      m_argSetId(argSetId_),
+      m_argId(argId_),
+      m_value(value_),
+      m_description(description_),
+      m_created(created_)
+{
+    m_effectArgsData = ::data_object::utils::createDataVector(static_cast<const EffectArgData *>(nullptr));
+}
+
+std::unique_ptr<EffectArgValueData> EffectArgValueData::getDataCopy() const
+{
+    std::unique_ptr<EffectArgValueData> result = std::make_unique<EffectArgValueData>();
+    result->m_id = m_id;
+    result->m_argSetId = m_argSetId;
+    result->m_argId = m_argId;
+    result->m_value = m_value;
+    result->m_description = m_description;
+    result->m_created = m_created;
+    return result;
+}
--- MastactvaModels/effectargvalue_data.h
+++ MastactvaModels/effectargvalue_data.h
@@ -2,4 +2,37 @@
 #define EFFECTARGVALUE_DATA_H
 
 
+#include <memory>
+#include <QString>
+#include <QDateTime>
+#include <QVector>
+#include "../MastactvaModels/effectarg_data.h"
+
+
+class EffectArgValueData
+{
+public:
+    EffectArgValueData();
+    EffectArgValueData(
+            int id_,
+            int argSetId_,
+            int argId_,
+            const QString &value_,
+            const QString &description_,
+            const QDateTime &created_
+            );
+    virtual ~EffectArgValueData() = default;
+    virtual std::unique_ptr<EffectArgValueData> getDataCopy() const;
+
+public:
+    int m_id = -1;
+    int m_argSetId = -1;
+    int m_argId = -1;
+    QString m_value;
+    QString m_description;
+    QDateTime m_created;
+    std::shared_ptr<QVector<EffectArgData *>> m_effectArgsData;
+};
+
+
 #endif // EFFECTARGVALUE_DATA_H
