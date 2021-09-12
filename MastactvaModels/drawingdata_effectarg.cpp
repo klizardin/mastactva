8551b6a6e362398fc2c00be42d14b2757693b696
sergey <klizardin@gmail.com>
2021-09-12 14:49:24 +0300

    create DrawingDataEffectArg class

--- MastactvaModels/drawingdata_effectarg.cpp
+++ MastactvaModels/drawingdata_effectarg.cpp
@@ -1,2 +1,8 @@
 #include "drawingdata_effectarg.h"
 
+
+DrawingDataEffectArg::DrawingDataEffectArg(EffectArgData &&data_)
+    : EffectArgData(std::move(data_))
+{
+
+}
--- MastactvaModels/drawingdata_effectarg.h
+++ MastactvaModels/drawingdata_effectarg.h
@@ -2,4 +2,17 @@
 #define DRAWINGDATA_EFFECTARG_H
 
 
+#include "../MastactvaModels/effectarg_data.h"
+#include "../MastactvaBase/drawingdata_utils.h"
+
+
+class DrawingDataEffectArg :
+        public EffectArgData
+{
+public:
+    DrawingDataEffectArg() = default;
+    DrawingDataEffectArg(EffectArgData &&data_);
+};
+
+
 #endif // DRAWINGDATA_EFFECTARG_H
