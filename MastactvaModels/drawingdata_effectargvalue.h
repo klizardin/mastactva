937bea5bf64d6337a6f97b82528d45793dfa867b
sergey <klizardin@gmail.com>
2021-09-12 14:53:53 +0300

    create DrawingDataEffectArgValue class

--- MastactvaModels/drawingdata_effectargvalue.h
+++ MastactvaModels/drawingdata_effectargvalue.h
@@ -2,4 +2,17 @@
 #define DRAWINGDATA_EFFECTARGVALUE_H
 
 
+#include "../MastactvaModels/effectargvalue_data.h"
+#include "../MastactvaBase/drawingdata_utils.h"
+
+
+class DrawingDataEffectArgValue :
+        public EffectArgValueData
+{
+public:
+    DrawingDataEffectArgValue() = default;
+    DrawingDataEffectArgValue(EffectArgValueData &&data_);
+};
+
+
 #endif // DRAWINGDATA_EFFECTARGVALUE_H
