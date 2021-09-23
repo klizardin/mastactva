71de2f4259ff7bd65dc723cd1a3d201f24e40221
sergey <klizardin@gmail.com>
2021-09-23 08:00:21 +0300

    add template code of class EffectsExchange

--- MastactvaQuizEditor/effects_exchange.cpp
+++ MastactvaQuizEditor/effects_exchange.cpp
@@ -1,2 +1,29 @@
 #include "effects_exchange.h"
 
+
+EffectsExchange::EffectsExchange(QObject *parent_ /*= nullptr*/)
+    : QObject(parent_)
+{
+}
+
+EffectsExchange::~EffectsExchange()
+{
+}
+
+void EffectsExchange::download()
+{
+}
+
+void EffectsExchange::upload()
+{
+}
+
+QString EffectsExchange::savePath() const
+{
+    return m_path;
+}
+
+void EffectsExchange::setSavePath(const QString &path_)
+{
+    m_path = path_;
+}
--- MastactvaQuizEditor/effects_exchange.h
+++ MastactvaQuizEditor/effects_exchange.h
@@ -2,4 +2,36 @@
 #define EFFECTS_EXCHANGE_H
 
 
+#include <QObject>
+#include <QtQuick/QQuickItem>
+
+
+class EffectsExchange : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+public:
+    explicit EffectsExchange(QObject *parent_ = nullptr);
+    virtual ~EffectsExchange() override;
+
+    Q_PROPERTY(QString savePath READ savePath WRITE setSavePath NOTIFY savePathChanged)
+    Q_INVOKABLE void download();
+    Q_INVOKABLE void upload();
+
+private:
+    QString savePath() const;
+    void setSavePath(const QString &path_);
+
+public:
+signals:
+    void savePathChanged();
+
+protected slots:
+
+private:
+    QString m_path;
+};
+
+
 #endif // EFFECTS_EXCHANGE_H
