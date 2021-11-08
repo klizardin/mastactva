1bfe59b3239a1f9d20856f2cdc93f25909f91c7c
sergey <klizardin@gmail.com>
2021-11-08 16:19:22 +0300

    change api

--- MastactvaModels/effectargument_data.h
+++ MastactvaModels/effectargument_data.h
@@ -21,15 +21,14 @@ public:
     virtual ~EffectArgumentData() = default;
     EffectArgumentData & operator = (EffectArgumentData &&data_);
     std::unique_ptr<EffectArgumentData> getDataCopy() const;
+    EffectArgumentData(const EffectArgumentData &data_) = default;
+    EffectArgumentData &operator = (const EffectArgumentData &data_) = default;
 
 public:
     int m_argStorageId = -1;
     int m_argTypeId = -1;
     QString m_name;
     QString m_defaultValue;
-
-    EffectArgumentData(const EffectArgumentData &data_) = delete;
-    EffectArgumentData &operator = (const EffectArgumentData &data_) = delete;
 };
 
 
