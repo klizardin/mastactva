eea965f548eb3e89ff77ccc4c7e3a0fcd2ff288c
sergey <klizardin@gmail.com>
2021-11-20 14:34:16 +0300

    extend FileSource API - add FileSourceReference class

--- MastactvaBase/imagesource.h
+++ MastactvaBase/imagesource.h
@@ -52,6 +52,92 @@ public:
     }
 };
 
+class FileSourceReference : private FileSource
+{
+public:
+    FileSourceReference(const QString &str_ = QString())
+    {
+        operator=(str_);
+    }
+
+    FileSourceReference(const FileSource &fileSource_)
+        :FileSource(fileSource_)
+    {
+    }
+
+    FileSourceReference(const FileSourceReference &fileSourceReference_)
+        :FileSource(static_cast<const FileSource &>(fileSourceReference_))
+    {
+    }
+
+    FileSourceReference &operator = (const QString& str_)
+    {
+        *static_cast<QString *>(this) = str_;
+        return *this;
+    }
+
+    FileSourceReference &operator = (const FileSource &fileSource_)
+    {
+        return operator=(static_cast<const QString&>(fileSource_));
+    }
+
+    FileSourceReference &operator = (const FileSourceReference &fileSourceReference_)
+    {
+        return operator=(static_cast<const FileSource &>(fileSourceReference_));
+    }
+
+    bool isInsideObject() const
+    {
+        return m_hasFile
+                && m_hasFileReference
+                && m_value
+                ;
+    }
+
+    void initObjectDetails(bool *hasFile_, bool *hasFileReference_, QString *value_)
+    {
+        m_hasFile = hasFile_;
+        m_hasFileReference = hasFileReference_;
+        m_value = value_;
+    }
+
+    const QString & value() const
+    {
+        if(isInsideObject()
+                && !*m_hasFile
+                && *m_hasFileReference
+                )
+        {
+            return *m_value;
+        }
+        else
+        {
+            return static_cast<const QString &>(*this);
+        }
+    }
+
+    bool hasFile() const
+    {
+        return isInsideObject() && *m_hasFile;
+    }
+
+    std::unique_ptr<QFile> getFile() const
+    {
+        if(!hasFile())
+        {
+            return nullptr;
+        }
+        QUrl url(static_cast<const QString &>(*this));
+        QString filename = url.toLocalFile();
+        return std::make_unique<QFile>(filename);
+    }
+
+private:
+    bool *m_hasFile = nullptr;
+    bool *m_hasFileReference = nullptr;
+    QString *m_value = nullptr;
+};
+
 
 namespace layout
 {
@@ -76,6 +162,36 @@ namespace layout
     }
 
     inline void setValue(const QJsonValue &var_, FileSource &dta_)
+    {
+        dta_ = var_.toString();
+    }
+
+    inline QVariant getValue(const FileSourceReference &val_, bool toQML_ = true)
+    {
+        if(toQML_ || !val_.hasFile())
+        {
+            return QVariant::fromValue(
+                        static_cast<const QString &>(
+                            val_.value()
+                            )
+                        );
+        }
+        else
+        {
+            return QVariant::fromValue(
+                        static_cast<QObject *>(
+                            val_.getFile().release()
+                            )
+                        );
+        }
+    }
+
+    inline void setValue(const QVariant &var_, FileSourceReference &dta_)
+    {
+        dta_ = qvariant_cast<QString>(var_);
+    }
+
+    inline void setValue(const QJsonValue &var_, FileSourceReference &dta_)
     {
         dta_ = var_.toString();
     }
