caeb813c5ab3a5873a2fe0694e2cb016b192adb0
sergey <klizardin@gmail.com>
2021-12-04 18:08:48 +0300

    add lua tests (in progress...)
    1) add data layout data

--- MastactvaTests/lua_unittests.h
+++ MastactvaTests/lua_unittests.h
@@ -5,14 +5,40 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 #include <QString>
+#include "lua_utils.h"
+#include "luaapi.h"
 #include "test_utils.h"
 
 
 using namespace testing;
 
 
+static const char *g_luaScriptBaseDataTestFmt =
+        "function main ()\n"
+        "    result = {}\n"
+        "    result[\"%2\"] = %3\n"
+        "    test(\"%1\", result)\n"
+        "end\n";
+
+struct DataTestData
+{
+    int a;
+    double b;
+    QString c;
+};
+
+static const auto g_DataTestDataLayout = makeDataLayout(
+            makeFieldLayout("a", &DataTestData::a),
+            makeFieldLayout("b", &DataTestData::b),
+            makeFieldLayout("c", &DataTestData::c)
+            );
+
+
 TEST(Lua, base)
 {
+    LuaAPI api;
+    //api.addTest()
+
     ASSERT_TRUE(true);
 }
 
