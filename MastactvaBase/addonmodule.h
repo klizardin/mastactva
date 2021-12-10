0c4bf5ba8084865f3b8b3e03cf630be955432e37
sergey <klizardin@gmail.com>
2021-12-10 07:16:18 +0300

    add interface definition

--- MastactvaBase/addonmodule.h
+++ MastactvaBase/addonmodule.h
@@ -2,4 +2,12 @@
 #define ADDONMODULE_H
 
 
+class IAddonModule
+{
+public:
+    virtual const char * getName() const = 0;
+    virtual const char * process(const char * arguments_) const = 0;
+};
+
+
 #endif // ADDONMODULE_H
