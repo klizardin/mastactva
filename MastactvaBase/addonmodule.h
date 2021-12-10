bacb821c61c194150cabfac054112b0e91a496ff
sergey <klizardin@gmail.com>
2021-12-10 08:27:30 +0300

    define interface of addonmodule

--- MastactvaBase/addonmodule.h
+++ MastactvaBase/addonmodule.h
@@ -2,14 +2,6 @@
 #define ADDONMODULE_H
 
 
-class IAddonModule
-{
-public:
-    virtual const char * getName() const = 0;
-    virtual const char * process(const char * arguments_) const = 0;
-};
-
-
 extern "C" const char * addonModuleGetName();
 extern "C" const char * addonModuleProcess(const char * );
 
