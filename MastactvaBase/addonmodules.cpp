59994020f5e587b9158f2f2bf53de28a44727a9a
sergey <klizardin@gmail.com>
2021-12-14 07:57:54 +0300

    add interface use

--- MastactvaBase/addonmodules.cpp
+++ MastactvaBase/addonmodules.cpp
@@ -1,14 +1,2 @@
 #include "addonmodules.h"
 
-
-#include <QStringList>
-#include <QJsonDocument>
-#include <QString>
-
-
-class IAddonModules
-{
-public:
-    virtual QStringList getNames() const = 0;
-    virtual QJsonDocument call(const QString &name_, const QJsonDocument &arguments_) const = 0;
-};
