a2a450de5bb6fe8da0e38a54c3d0a05f1bdc870a
sergey <klizardin@gmail.com>
2022-02-13 17:00:00 +0300

    add constant

--- MastactvaBase/names.h
+++ MastactvaBase/names.h
@@ -131,6 +131,7 @@ Q_DECL_UNUSED static const char * g_renderObjectsStateStartName = "renderObjectS
 Q_DECL_UNUSED static const char * g_renderGlobalStateStartName = "renderGlobalState";
 Q_DECL_UNUSED static const char * g_renderGlobalCalculationsName = "renderGlobalCalculations";
 Q_DECL_UNUSED static const char * g_renderObjectCalculationsName = "renderObjectCalculations";
+Q_DECL_UNUSED static const char * g_renderCalculationsSpliter = ",";
 
 
 // db request info
