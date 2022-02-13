c8e1358144b88091b3a47734d88261aa677bc9f4
sergey <klizardin@gmail.com>
2022-02-13 17:00:48 +0300

    walk effect drawing calculations (in progress..)

--- MastactvaBase/opengldrawing_calculations.h
+++ MastactvaBase/opengldrawing_calculations.h
@@ -2,5 +2,32 @@
 #define OPENGLDRAWING_CALCULATIONS_H
 
 
+#include "../MastactvaBase/opengldrawing_utils.h"
+
+
+namespace opengl_drawing
+{
+
+
+class WalkEffectRectMatrixCalculation
+        : public IEffectCalculation
+{
+public:
+    WalkEffectRectMatrixCalculation();
+    ~WalkEffectRectMatrixCalculation() override;
+
+    bool init(const QString &args_);
+
+    void calculate(IVariables *variables_) const override;
+
+private:
+    QString m_vertexAttributeName;
+    QString m_renderImageName;
+    QString m_rectMatrixUniformName;
+};
+
+
+} // namespace opengl_drawing
+
 
 #endif // OPENGLDRAWING_CALCULATIONS_H
