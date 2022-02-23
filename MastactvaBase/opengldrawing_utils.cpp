25ddbd8348d7316322b7128e591c3c144c190e45
sergey <klizardin@gmail.com>
2022-02-23 14:14:26 +0300

    fix errors

--- MastactvaBase/opengldrawing_utils.cpp
+++ MastactvaBase/opengldrawing_utils.cpp
@@ -196,13 +196,15 @@ QMatrix4x4 calculatePreserveAspectFitTextureMatrix(
 
     if(rectRate >= imageRate)
     {
-        const float shift = (rectRate - imageRate) * 0.5f / rectRate;
+        const float shift = (1.0 - imageRate/rectRate) * 0.5f;
+        qDebug() << "shift" << shift;
         x0 += shift;
         x1 -= shift;
     }
     else
     {
-        const float shift = (rectRate - imageRate) * 0.5f / rectRate;
+        const float shift = (1.0 - rectRate/imageRate) * 0.5f;
+        qDebug() << "shift" << shift;
         y0 += shift;
         y1 -= shift;
     }
@@ -221,7 +223,7 @@ QMatrix4x4 calculatePreserveAspectFitTextureMatrix(
         {x1, y0}
     };
     const QMatrix4x4 m1 = calculateTransfromMatrixBy4Points(srcPts, destPts);
-
+    qDebug() << "m1" << m1 << "destPts" << destPts;
     return m1 * textureMatrix;
 }
 
