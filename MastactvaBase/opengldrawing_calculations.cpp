b34b18f7b5ae33a042324e443883324cef9130f0
sergey klizardin <klizardin@gmail.com>
2022-05-02 07:45:46 +0300

    add implementation - clip rect calculation (in progress...)

--- MastactvaBase/opengldrawing_calculations.cpp
+++ MastactvaBase/opengldrawing_calculations.cpp
@@ -196,6 +196,7 @@ void opengl_drawing::WalkEffectRectMatrixCalculation::calculate(opengl_drawing::
 
 opengl_drawing::WalkEffectClipRectCalculation::WalkEffectClipRectCalculation()
 {
+    setFilename(g_renderWalkEffectClipRectCalculation);
 }
 
 opengl_drawing::WalkEffectClipRectCalculation::~WalkEffectClipRectCalculation()
@@ -204,11 +205,66 @@ opengl_drawing::WalkEffectClipRectCalculation::~WalkEffectClipRectCalculation()
 
 bool opengl_drawing::WalkEffectClipRectCalculation::init(const QString &args_)
 {
-    return false;
+    const QStringList args = removeEmpty(trimmed(args_.split(g_argumentsSplitter)));
+    if(args.size() < 2)
+    {
+        return false;
+    }
+    m_renderFromImageName = args[0].trimmed();
+    m_renderToImageName = args[1].trimmed();
+
+    setRequiredVariables({
+                             g_renderWindowSizeName,
+                             m_renderFromImageName,
+                             m_renderToImageName
+                         });
+    return !m_renderFromImageName.isEmpty()
+            && !m_renderToImageName.isEmpty()
+            ;
 }
 
 void opengl_drawing::WalkEffectClipRectCalculation::calculate(IVariables *variables_) const
 {
+    opengl_drawing::Objects *objects = variables_ ? dynamic_cast<opengl_drawing::Objects *>(variables_->getRoot()) : nullptr;
+    if(!objects)
+    {
+        return;
+    }
 
+    const QVector2D windowSizeF = objects->getUniform(g_renderWindowSizeName, QVector2D{1.0,1.0});
+    const QSize windowSize((int)windowSizeF.x(), (int)windowSizeF.y());
+    const QVector4D clipRectFromImage = getImageClipRect(objects, m_renderFromImageName, windowSize );
+    const QVector4D clipRectToImage = getImageClipRect(objects, m_renderToImageName, windowSize );
+    const QVector4D clipRect = minClipRect(clipRectFromImage, clipRectToImage);
+
+    qDebug() << clipRect;
+    objects->setUniform(
+                QString(g_renderObjectsStateStartName) + QString(g_renderClipRectStateName),
+                clipRect
+                );
 }
+
+QVector4D opengl_drawing::WalkEffectClipRectCalculation::getImageClipRect(
+        opengl_drawing::Objects *objects_,
+        const QString &imageName_,
+        const QSize &windowSize_
+        ) const
+{
+    const QSize imageSize = objects_->getTextureSize(imageName_ , windowSize_);
+    const QMatrix4x4 m = ::opengl_drawing::calculatePreserveAspectFitTextureMatrix(imageSize, windowSize_, true);
+    const QVector4D topLeftPt{0.0, 0.0, 0.0, 1.0};
+    const QVector4D bottomRightPt{(float)windowSize_.width(), (float)windowSize_.height(), 0.0, 1.0};
+    const QVector4D topLeftPtT = m * topLeftPt;
+    const QVector4D bottomRightPtT = m * bottomRightPt;
+    return QVector4D{topLeftPtT.x(), topLeftPtT.y(), bottomRightPtT.x(), bottomRightPtT.y()};
+}
+
+QVector4D opengl_drawing::WalkEffectClipRectCalculation::minClipRect(const QVector4D &cr1_, const QVector4D &cr2_) const
+{
+    return QVector4D{
+        std::max(cr1_.x(), cr2_.x()), std::max(cr1_.y(), cr2_.y()),
+        std::min(cr1_.z(), cr2_.z()), std::min(cr1_.w(), cr2_.w())
+        };
+}
+
 
