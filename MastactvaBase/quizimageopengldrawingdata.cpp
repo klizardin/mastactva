feccede9e277505a96e90dca0d78ebbe91ef6c1c
sergey klizardin <klizardin@gmail.com>
2022-05-16 06:59:48 +0300

    fix bug - fix incomplete operation

--- MastactvaBase/quizimageopengldrawingdata.cpp
+++ MastactvaBase/quizimageopengldrawingdata.cpp
@@ -298,6 +298,7 @@ public:
         {
             glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         }
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
     void release(const QString &stateStr_, const std::vector<GLfloat> &) override
