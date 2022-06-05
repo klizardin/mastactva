46e56e64e7eb10e1a62eabe8503697274a3c65ab
sergey klizardin <klizardin@gmail.com>
2022-06-05 11:24:29 +0300

    allow to add calculation only objects to the drawing objects

--- MastactvaModels/drawingdata_effectobjects.cpp
+++ MastactvaModels/drawingdata_effectobjects.cpp
@@ -196,8 +196,9 @@ bool ObjectArtefacts::build(
             (*it)->runAddons(details_, addonNames_);
         }
     }
-    return !object_.fragmentShader.isEmpty()
-            && !object_.vertexShader.isEmpty()
+    return (!object_.fragmentShader.isEmpty()
+            && !object_.vertexShader.isEmpty())
+            || object_.hasCalculations()
             ;
 }
 
