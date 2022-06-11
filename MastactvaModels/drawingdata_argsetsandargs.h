bcd6802d33ef0c69ccadb708eed8140994111c0a
sergey klizardin <klizardin@gmail.com>
2022-06-11 15:15:29 +0300

    add comments

--- MastactvaModels/drawingdata_argsetsandargs.h
+++ MastactvaModels/drawingdata_argsetsandargs.h
@@ -21,13 +21,23 @@ public:
     std::shared_ptr<QVector<EffectArgData *>> m_effectArgsData;
     std::shared_ptr<QVector<EffectArgSetData *>> m_effectArgSetsData;
 
+    // main interface
+    // find arguemnt by name for the current object in the arg sets
+    // function is not const because it sets value if it has found any
     bool find(const QString &name_);
+    // return value of founded argument
     QString getValue() const;
 
+    // choose current arg set
     void setArgSetIndex(int index_);
+    // set object local position
+    // TODO: why we need object local position flag
     void setObjectLocalPosition(bool local_);
+    // is local objct position set
     bool doAddVariableToLocalPosition() const;
+    // set object artefcat id
     void setObjectArtefactId(int objectArtefactId_);
+    // clear object artefact id
     void clearObjectArtefactId();
 
 private:
