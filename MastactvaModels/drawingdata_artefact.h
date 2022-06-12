514d11a98d19630bbe225640322e9839854378f3
sergey klizardin <klizardin@gmail.com>
2022-06-12 15:58:15 +0300

    add comments

--- MastactvaModels/drawingdata_artefact.h
+++ MastactvaModels/drawingdata_artefact.h
@@ -25,49 +25,122 @@
 #include "../MastactvaBase/drawingdata_utils.h"
 
 
+/*
+ * interface for drawing data artefact
+*/
 class IDrawingDataArtefact
 {
 public:
     virtual ~IDrawingDataArtefact() = default;
+
+    /*
+     * set vertex shader to the object if the artefact is vertex shader
+    */
     virtual bool setVertexShader(
             drawing_data::QuizImageObject &object_,
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * set fragment shader to the object if the artefact is fragment shader
+    */
     virtual bool setFragmentShader(
             drawing_data::QuizImageObject &object_,
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * add arguments of the artefact if any
+     * add values of arguments to the object values
+     * if there is no variable for this argument
+    */
     virtual void addArguments(
             drawing_data::QuizImageObject &object_,
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * return true if the artefact has arguments
+    */
     virtual bool hasArguments() const = 0;
+
+    /*
+     * return true if the artefact has global arguments
+     * arguments that is used for global drawing object states
+    */
     virtual bool hasGlobalArguments() const = 0;
+
+    /*
+     * return true if the artefact has variables
+     * variables are the values that sets not to the object but to the
+     * variables. variables can be used by scripts by arguments
+     * argument of shader is the default value that uses if there is no
+     * variable value for this argument
+     * lua scripts setup variables and use variables
+    */
     virtual bool hasVariables() const = 0;
+
+    /*
+     * add variables if there is any
+    */
     virtual void addVariables(
             const drawingdata::Details &details_,
             DrawingDataArgSetsAndArgs *argSetsAndArgs_ = nullptr
             ) const = 0;
+
+    /*
+     * add global arguments
+     * (special setup) can be specified just for main objects
+    */
     virtual void addGlobalArguments(
             drawing_data::QuizImageObjects &data_,
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * add texture if the artefact is a texture
+    */
     virtual void addTexture(
             drawing_data::QuizImageObject &object_
             ) const = 0;
+
+    /*
+     * add some type of data to the drawing object
+     * if the artefact is a some kind of data
+    */
     virtual void addData(
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * add calculations if the artefact is a calculation
+    */
     virtual void addCalculations(
             drawing_data::QuizImageObject &object_,
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * add calculations if the artefact is a calculation
+    */
     virtual void addMainCalculations(
             drawing_data::QuizImageObjects &objects_,
             const drawingdata::Details &details_
             ) const = 0;
+
+    /*
+     * do artefact is an addon (GUI addons by IEffectAddOn interface)
+    */
     virtual bool hasAddon() const = 0;
+
+    /*
+     * return addon's names
+    */
     virtual void getAddonNames(QStringList &names_) const = 0;
+
+    /*
+     * run GUI addons
+    */
     virtual void runAddons(
             const drawingdata::Details &details_,
             const QStringList &addonNames_
@@ -75,6 +148,9 @@ public:
 };
 
 
+/*
+ * implementation of the interface IDrawingDataArtefact for the data ArtefactData
+*/
 class DrawingDataArtefact :
         public ArtefactData,
         public IDrawingDataArtefact
