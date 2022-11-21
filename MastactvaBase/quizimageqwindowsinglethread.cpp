c616d733217568a24bd577e6ae8f0b6a0bf12526
sergey klizardin <klizardin@gmail.com>
2022-11-21 07:30:13 +0300

    try to use QuizImageQWindowSingleThread as a main window

--- MastactvaBase/quizimageqwindowsinglethread.cpp
+++ MastactvaBase/quizimageqwindowsinglethread.cpp
@@ -286,6 +286,7 @@ bool QuizImageQWindowSingleThread::QuizImageQMLDrawingSurface::hasTexture() cons
 
 bool QuizImageQWindowSingleThread::QuizImageQMLDrawingSurface::startQuick(const QString &filename)
 {
+    m_qmlEngine->addImportPath("qrc:/Mastactva");
     m_qmlComponent = std::make_unique<QQmlComponent>(m_qmlEngine.get(), QUrl(filename));
     return !m_qmlComponent->isLoading();
 }
