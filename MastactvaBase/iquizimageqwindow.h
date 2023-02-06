036e8b4cc581ae7e35af16032422729b079662df
sergey klizardin <klizardin@gmail.com>
2023-02-06 06:57:32 +0300

    extend API

--- MastactvaBase/iquizimageqwindow.h
+++ MastactvaBase/iquizimageqwindow.h
@@ -11,6 +11,9 @@ class IQuizImageQWindow
 public:
     virtual void add(const std::vector<QString> & textures) = 0;
     virtual int count() const = 0;
+
+public:
+    static IQuizImageQWindow * getInstance();
 };
 
 
