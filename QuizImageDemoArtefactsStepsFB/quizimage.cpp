8ba495a00c4720f103e887887734b4cb658566d8
sergey klizardin <klizardin@gmail.com>
2023-03-20 07:44:55 +0300

    add inititialization of the drawing data (in progress...)

--- QuizImageDemoArtefactsStepsFB/quizimage.cpp
+++ QuizImageDemoArtefactsStepsFB/quizimage.cpp
@@ -248,12 +248,15 @@ void QuizImage::initDefaultDrawingData()
      * So here we add all, but interface will filter calls
     */
 
-    //if(IQuizImageQWindow::getInstance()
-    //        && IQuizImageQWindow::getInstance()->getDrawingData()
-    //        )
-    //{
-    //    //m_drawingData = IQuizImageQWindow::getInstance()->getDrawingData();
-    //}
+    IQuizImageQWindowOperations *quizImageOperations = IQuizImageQWindow::findQuizImageWindows(renderingWindowsId());
+    if(quizImageOperations
+            && !isDefaultTexture()
+            && quizImageOperations->getDrawingData()
+            )
+    {
+        //m_drawingData = quizImageOperations->getDrawingData();
+        //return;
+    }
 
     if(m_testIndex < 0)
     {
@@ -271,11 +274,12 @@ void QuizImage::initDefaultDrawingData()
         m_drawingData = std::move(data);
     }
 
-    //if(IQuizImageQWindow::getInstance())
-    //{
-    //    //IQuizImageQWindow::getInstance()->setDrawingData(m_drawingData);
-    //    IQuizImageQWindow::getInstance()->setTextures(m_drawingData->getTargetTextures());
-    //}
+    if(quizImageOperations
+            && isDefaultTexture())
+    {
+        quizImageOperations->setDrawingData(m_drawingData);
+        //quizImageOperations->setTextures(m_drawingData->getTargetTextures());
+    }
 }
 
 bool QuizImage::isFromImageReady() const
