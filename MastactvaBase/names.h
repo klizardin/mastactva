8576bf3a571f68bfe1d14eb6f493df8dfecfce9b
sergey klizardin <klizardin@gmail.com>
2024-04-18 06:53:28 +0300

    add name

--- MastactvaBase/names.h
+++ MastactvaBase/names.h
@@ -107,6 +107,7 @@ Q_DECL_UNUSED static const char * g_renderFromImageName = "renderFromImage";
 Q_DECL_UNUSED static const char * g_renderToImageName = "renderToImage";
 Q_DECL_UNUSED static const char * g_renderMatrixName = "renderMatrix";
 Q_DECL_UNUSED static const char * g_renderImageMatrixName = "renderFromMatrix";
+Q_DECL_UNUSED static const char * g_renderImageMatrixBaseName = "renderImageMatrix";
 Q_DECL_UNUSED static const char * g_renderFromImageMatrixName = "renderFromImageMatrix";
 Q_DECL_UNUSED static const char * g_renderToImageMatrixName = "renderToImageMatrix";
 Q_DECL_UNUSED static const char * g_renderScreenRectName = "renderScreenRect";
