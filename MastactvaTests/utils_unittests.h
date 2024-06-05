68a5115a0708bc64051c60ab3dc7dcf595863679
sergey klizardin <klizardin@gmail.com>
2024-06-05 06:29:35 +0300

    add unit tests implemetation

--- MastactvaTests/utils_unittests.h
+++ MastactvaTests/utils_unittests.h
@@ -10,6 +10,41 @@
 
 TEST(Utils,ScalledTime)
 {
+    ScalledTime scalledTime;
+    QVariantList value;
+    value << QVariant::fromValue(0.0);
+    value << QVariant::fromValue(1000.0);
+    value << QVariant::fromValue(1.0);
+    value << QVariant::fromValue(1000.0);
+    value << QVariant::fromValue(0.0);
+    value << QVariant::fromValue(1000.0);
+    value << QVariant::fromValue(1.0);
+    value << QVariant::fromValue(1000.0);
+    value << QVariant::fromValue(0.0);
+
+    scalledTime.setVectorValue(value);
+    EXPECT_EQ(value, scalledTime.getVectorValue());
+
+    qreal dt = 0.0;
+    for(int i = 0; i < 10; i++)
+    {
+        ASSERT_DOUBLE_EQ(dt, 0.0);
+        ASSERT_DOUBLE_EQ(scalledTime.get(100.0*(i+1), dt), 0.1*(i+1));
+    }
+    ASSERT_DOUBLE_EQ(dt, 1000.0);
+    /*EXPECT_EQ(scalledTime.get(1000.0, dt), 0.0);
+    EXPECT_EQ(dt, 1000.0);
+    for(int i = 0; i < 10; i++)
+    {
+        EXPECT_EQ(scalledTime.get(100.0, dt), 0.1*(i+1));
+        EXPECT_EQ(dt, 0.1);
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        EXPECT_EQ(scalledTime.get(100.0, dt), 1.0 - 0.1*(i+1));
+        EXPECT_EQ(dt, 0.1);
+    }
+    std::cout << "Test" << std::endl;*/
 }
 
 #endif // UTILS_UNITTESTS_H
