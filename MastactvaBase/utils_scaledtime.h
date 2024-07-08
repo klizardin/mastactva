4707d3f535839fee82e2af21e5c6e2bb0bece53d
sergey klizardin <klizardin@gmail.com>
2024-07-08 07:03:12 +0300

    extend API

--- MastactvaBase/utils_scaledtime.h
+++ MastactvaBase/utils_scaledtime.h
@@ -6,6 +6,13 @@
 #include <qvector.h>
 
 
+struct IScaledTimeEvents
+{
+    virtual void onScaledTimeCycle(int cycle) const = 0;
+    virtual void onScaledTimeLastCycle() const = 0;
+};
+
+
 class ScaledTime
 {
 public:
@@ -16,6 +23,7 @@ public:
     int getCycles() const;
     void setCycles(int cycles);
     qreal get(qreal dt_, qreal& tIntermediate_);
+    void set(IScaledTimeEvents* events);
 
 private:
     qreal get(qreal dt_, int& delayIndex_, qreal& tIntermediate_);
@@ -26,6 +34,8 @@ private:
     int m_currentTScalesVectorIndex = 0;
     qreal m_lastTime = 0.0;
     int m_cycles = 0;
+    int m_cyclesCount = 0;
+    IScaledTimeEvents* m_events = nullptr;
 };
 
 
