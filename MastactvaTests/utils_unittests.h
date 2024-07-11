a4b21cb34c54c731cff417acb895d733638a3928
sergey klizardin <klizardin@gmail.com>
2024-07-11 06:55:16 +0300

    update tests

--- MastactvaTests/utils_unittests.h
+++ MastactvaTests/utils_unittests.h
@@ -135,12 +135,12 @@ TEST(Utils, ScaledTime_Cycles_mock)
     for(int i = 0; i < cycles; ++i)
     {
         (void)scalledTime.get(1000.0, dt);
-        (void)scalledTime.get(1000.0, dt);
         EXPECT_CALL(events_mock, onScaledTimeCycle(i)).Times(testing::AtLeast(1));
+        (void)scalledTime.get(1000.0, dt);
     }
-    (void)scalledTime.get(1000.0, dt);
-    (void)scalledTime.get(1000.0, dt);
     EXPECT_CALL(events_mock, onScaledTimeLastCycle()).Times(testing::AtLeast(1));
+    (void)scalledTime.get(1000.0, dt);
+    (void)scalledTime.get(1000.0, dt);
 }
 
 #endif // UTILS_UNITTESTS_H
