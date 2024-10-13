24038847a36770e42d1228b18af2f208d7ce64db
sergey klizardin <klizardin@gmail.com>
2024-10-13 15:43:45 +0300

    comment traces, return work constant

--- QuizImageDemoArtefactsStepsFB/quizimage.cpp
+++ QuizImageDemoArtefactsStepsFB/quizimage.cpp
@@ -150,7 +150,7 @@ void QuizImage::setTScalesVector(const QVariantList& delaysVector)
 {
     m_scalledTime.setVectorValue(delaysVector);
     m_millisecondsSinceEpoche = QDateTime::currentMSecsSinceEpoch();
-    m_timerForT->start(1000);
+    m_timerForT->start(10);
 
     emit tScalesVectorChanged();
 }
