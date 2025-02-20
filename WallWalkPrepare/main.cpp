8a56f9e0eecc07d82f90904320bf24ab7a5bf7a5
sergey klizardin <klizardin@gmail.com>
2025-02-20 07:01:01 +0300

    add getting of pairs

--- WallWalkPrepare/main.cpp
+++ WallWalkPrepare/main.cpp
@@ -2,6 +2,8 @@
 #include <QLocale>
 #include <QTranslator>
 #include <QCommandLineParser>
+#include <QDir>
+#include <QStringList>
 
 
 int main(int argc, char *argv[])
@@ -43,7 +45,30 @@ int main(int argc, char *argv[])
     const QString sourceDir = cmdsParser.value(sourceDirOption);
     const int bunchSize = cmdsParser.value(bunchSizeOption).toInt();
 
-    qInfo() << "Source directory: " << sourceDir << " bunch size: " << bunchSize << "\n";
+    qInfo() << "Source directory: " << sourceDir << " bunch size: " << bunchSize;
+
+    QDir sourceImageDir(sourceDir);
+    QStringList images = sourceImageDir.entryList(QStringList() << "*.jpg" << "*.JPG", QDir::Files, QDir::Name);
+    QVector<QStringList> bunchOfImages;
+    bunchOfImages.push_back(QStringList());
+    int i = 0;
+    for(const QString& imageName : images)
+    {
+        if(++i>=bunchSize)
+        {
+            i = 0;
+            bunchOfImages.push_back(QStringList());
+        }
+        bunchOfImages.back().push_back(imageName);
+    }
+    qInfo() << "Bunch of images: " << bunchOfImages;
+    for(i = 0; i < bunchSize; i++)
+    {
+        for(int j = 1; j < bunchOfImages.size(); ++j)
+        {
+            qInfo() << "pair (" << j << "," << i<< "):" << bunchOfImages[j-1].at(i) << "-" << bunchOfImages[j].at(i);
+        }
+    }
 
     //return a.exec();
     return 0;
