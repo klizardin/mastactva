8d4f63854323b4360fd6c4dce2a7655237d25cb6
sergey klizardin <klizardin@gmail.com>
2025-02-21 07:33:15 +0300

    update algorithm

--- WallWalkPrepare/main.cpp
+++ WallWalkPrepare/main.cpp
@@ -54,9 +54,9 @@ int main(int argc, char *argv[])
     int i = 0;
     for(const QString& imageName : images)
     {
-        if(++i>=bunchSize)
+        if(++i>bunchSize)
         {
-            i = 0;
+            i = 1;
             bunchOfImages.push_back(QStringList());
         }
         bunchOfImages.back().push_back(imageName);
@@ -66,7 +66,14 @@ int main(int argc, char *argv[])
     {
         for(int j = 1; j < bunchOfImages.size(); ++j)
         {
-            qInfo() << "pair (" << j << "," << i<< "):" << bunchOfImages[j-1].at(i) << "-" << bunchOfImages[j].at(i);
+            qInfo() << "pair (" << (j-1)*bunchSize + i << "," << j*bunchSize + i << "):" << bunchOfImages[j-1].at(i) << "-" << bunchOfImages[j].at(i);
+        }
+    }
+    for(int j = 0; j < bunchOfImages.size(); ++j)
+    {
+        for(i = 1; i < bunchSize; i++)
+        {
+            qInfo() << "pair (" << j*bunchSize + i - 1 << "," << j*bunchSize + i << "):" << bunchOfImages[j].at(i - 1) << "-" << bunchOfImages[j].at(i);
         }
     }
 
