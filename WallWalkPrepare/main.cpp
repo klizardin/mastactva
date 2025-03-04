8112123fada1bdf5b9018d7737b5dbc52ec64c73
sergey klizardin <klizardin@gmail.com>
2025-03-04 07:43:21 +0300

    add the saving of the results

--- WallWalkPrepare/main.cpp
+++ WallWalkPrepare/main.cpp
@@ -6,6 +6,9 @@
 #include <QStringList>
 #include <QSet>
 #include <QPair>
+#include <QVector4D>
+#include <QJsonObject>
+#include <QJsonArray>
 #include "../MastactvaBase/addonmodulelist.h"
 #include "../MastactvaBase/utils.h"
 #include "../MastactvaBase/names.h"
@@ -55,6 +58,38 @@ static const char * g_inputJson =
             "\"output_rows\":10,"
             "\"output_cols\":10"
             "}";
+
+void convertJsonResultToCoordinates(const QJsonObject &object_, std::vector<QVector4D> &values_, QJsonArray& resultPack)
+{
+    QJsonArray empty;
+    resultPack.swap(empty);
+    values_.clear();
+    for(int i = 0;;i++)
+    {
+        if(!object_.contains(QString::number(i)))
+        {
+            return;
+        }
+        QVector4D vec;
+        const QJsonObject val = object_.value(QString::number(i)).toObject();
+        if(!val.contains("x") || !val.contains("y"))
+        {
+            return;
+        }
+        QJsonObject resultVal;
+        vec.setX(val.value("x").toDouble());
+        vec.setY(val.value("y").toDouble());
+        vec.setZ(-0.5);
+        vec.setW(1.0);
+        values_.push_back(vec);
+        resultVal.insert("x", QJsonValue::fromVariant(QVariant::fromValue(vec.x())));
+        resultVal.insert("y", QJsonValue::fromVariant(QVariant::fromValue(vec.y())));
+        resultVal.insert("z", QJsonValue::fromVariant(QVariant::fromValue(vec.z())));
+        resultVal.insert("w", QJsonValue::fromVariant(QVariant::fromValue(vec.w())));
+        resultPack.append(resultVal);
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -141,7 +176,11 @@ int main(int argc, char *argv[])
     auto modules = std::make_shared<AddonModules>();
     modules->create(addonsDir);
 
+    QJsonDocument result;
+    QJsonArray jsonPairs;
+
     qInfo() << "All pairs : " << pairs.size();
+    int index = 0;
     for(const auto& pair : pairs)
     {
         const QString inputJson = QString(g_inputJson).arg(
@@ -149,11 +188,59 @@ int main(int argc, char *argv[])
                     sourceImageDir.filePath(pair.second),
                     absoluteHomePath("~/tmp/")
                     );
-        qInfo() << pair.first << "," << pair.second;
+        qInfo() << index << ":" << pair.first << "," << pair.second;
 
         QJsonDocument result = modules->call("WalkEffect", QJsonDocument::fromJson(inputJson.toUtf8()));
-        qInfo() << result.isObject();
+
+        std::vector<QVector4D> fromValues, toValues;
+        QJsonArray fromValuesArray, toValuesArray;
+        convertJsonResultToCoordinates(result.object().value("1").toObject(), fromValues, fromValuesArray);
+        convertJsonResultToCoordinates(result.object().value("0").toObject(), toValues, toValuesArray);
+
+        QJsonObject jsonPair;
+        if(fromValues.empty() || toValues.empty())
+        {
+            const QString inputJson2 = QString(g_inputJson).arg(
+                        sourceImageDir.filePath(pair.second),
+                        sourceImageDir.filePath(pair.first),
+                        absoluteHomePath("~/tmp/")
+                        );
+            qInfo() << index << ":" << pair.second << "," << pair.first;
+
+            QJsonDocument result = modules->call("WalkEffect", QJsonDocument::fromJson(inputJson.toUtf8()));
+
+            convertJsonResultToCoordinates(result.object().value("0").toObject(), fromValues, fromValuesArray);
+            convertJsonResultToCoordinates(result.object().value("1").toObject(), toValues, toValuesArray);
+
+            if(!fromValues.empty() && !toValues.empty())
+            {
+                jsonPair.insert(g_demoJsonFieldFromFile, pair.second);
+                jsonPair.insert(g_demoJsonFieldToFile, pair.first);
+            }
+        }
+        else
+        {
+            jsonPair.insert(g_demoJsonFieldFromFile, pair.first);
+            jsonPair.insert(g_demoJsonFieldToFile, pair.second);
+        }
+
+        if(!fromValues.empty() && !toValues.empty())
+        {
+            jsonPair.insert(g_demoJsonFieldFromValues, fromValuesArray);
+            jsonPair.insert(g_demoJsonFieldToValues, toValuesArray);
+            jsonPairs.append(jsonPair);
+        }
+        else
+        {
+            qWarning() << "not processed!" << index << ":" << pair.second << "," << pair.first;
+        }
+        ++index;
     }
+
+    result.setArray(jsonPairs);
+    QFile resultFile(sourceImageDir.filePath(g_demoConfigurationJsonFileName));
+    resultFile.open(QFile::WriteOnly);
+    resultFile.write(result.toJson());
 
     //return a.exec();
     return 0;
