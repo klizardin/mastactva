3c8070c1de95ae3dce398b8527de1d07c56e63ab
sergey klizardin <klizardin@gmail.com>
2026-03-10 07:42:52 +0300

    add image names preprocessing

--- WallWalkDemoSimple/main.cpp
+++ WallWalkDemoSimple/main.cpp
@@ -4,6 +4,7 @@
 #include <QCommandLineParser>
 #include <QJsonDocument>
 #include <QFile>
+#include <QDir>
 #include "../MastactvaBase/quizimageqwindowsinglethreaddemo.h"
 #include "../MastactvaBase/names.h"
 
@@ -47,6 +48,47 @@ int main(int argc, char *argv[])
     const int bunchSize = cmdsParser.value(bunchSizeOption).toInt();
 
     qInfo() << "Source directory: " << sourceDir << " bunch size: " << bunchSize;
+
+    QDir sourceImageDir(sourceDir);
+    QStringList images = sourceImageDir.entryList(QStringList() << "*.jpg" << "*.JPG", QDir::Files, QDir::Name);
+    QVector<QStringList> bunchOfImages;
+    bunchOfImages.push_back(QStringList());
+    int i = 0;
+    for(const QString& imageName : images)
+    {
+        if(++i>bunchSize)
+        {
+            i = 1;
+            bunchOfImages.push_back(QStringList());
+        }
+        bunchOfImages.back().push_back(imageName);
+    }
+    //qDebug() << "Bunch of images: " << bunchOfImages;
+    QVector<QPair<QString, QString>> pairs;
+    for(i = 0; i < bunchSize; i++)
+    {
+        for(int j = 1; j < bunchOfImages.size(); ++j)
+        {
+            //qDebug() << "pair (" << (j-1)*bunchSize + i << "," << j*bunchSize + i << "):" << bunchOfImages[j-1].at(i) << "-" << bunchOfImages[j].at(i);
+            if(i >= 0 && i < bunchOfImages[j-1].size() && i < bunchOfImages[j].size())
+            {
+                pairs.push_back(qMakePair(bunchOfImages[j-1].at(i),bunchOfImages[j].at(i)));
+            }
+        }
+    }
+    for(int j = 0; j < bunchOfImages.size(); ++j)
+    {
+        for(i = 1; i < bunchSize; i++)
+        {
+            //qDebug() << "pair (" << j*bunchSize + i - 1 << "," << j*bunchSize + i << "):" << bunchOfImages[j].at(i - 1) << "-" << bunchOfImages[j].at(i);
+            if(i>=1 && i < bunchOfImages[j].size())
+            {
+                pairs.push_back(qMakePair(bunchOfImages[j].at(i-1),bunchOfImages[j].at(i)));
+            }
+        }
+    }
+
+    qInfo() << "All pairs : " << pairs.size();
 
     QJsonDocument configuration;
     QFile configurationFile(g_demoConfigurationJsonFileName);
