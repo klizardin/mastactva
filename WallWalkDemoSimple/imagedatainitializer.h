5e0281ccd310260d0013642b62a47d09bb8d667a
sergey klizardin <klizardin@gmail.com>
2026-03-18 07:36:09 +0300

    add implementation to create effect data

--- WallWalkDemoSimple/imagedatainitializer.h
+++ WallWalkDemoSimple/imagedatainitializer.h
@@ -37,8 +37,13 @@ class ImageDataInitializer : public IDefaultData<drawing_data::QuizImageObjects>
 public:
     ImageDataInitializer();
     //void setInfo(const QString& filenameA_, const QString& filenameB_, ... );
-    //void initialize(drawing_data::QuizImageObjects &data_, int argsSetIndex_ = 0) const override;
-    //std::pair<const char *, const char *> getDescription() const override;
+    void initialize(drawing_data::QuizImageObjects &data_, int argsSetIndex_ = 0) const override;
+    std::pair<const char *, const char *> getDescription() const override;
+
+private:
+    std::shared_ptr<MapFileSource> m_filesource;
+    const QPair<QString,QString> m_filenames;
+    const QPair<std::vector<QVector4D>, std::vector<QVector4D>> m_coordinates;
 };
 
 #endif // IMAGEDATAINITIALIZER_H
